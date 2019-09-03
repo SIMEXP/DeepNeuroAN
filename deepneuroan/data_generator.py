da86e4a3f59a9a81328bf6f66165f9fbcd5994e1
ltetrel <loic.tetrel.pro@gmail.com>
2019-09-03 13:50:17 -0400

    fix bug with shuffling for reproducibility
    added data normalization as in IntelAI/unet/3D/dataloader.py
    
    few optimizations

--- deepneuroan/data_generator.py
+++ deepneuroan/data_generator.py
@@ -5,7 +5,7 @@ import SimpleITK as sitk
 
 
 def load_file(path):
-    'load a transformation file into a quaternion + translation (mm) numpy array'
+    """load a transformation file into a quaternion + translation (mm) numpy array"""
     q = None
     match_float = "[+-]?[0-9]*[.]?[0-9]+"
     to_match = "(" + match_float + ") \t "\
@@ -19,102 +19,127 @@ def load_file(path):
         for line in fst:
             if re.match(to_match, line):
                 match = re.match(to_match, line)
-                q = np.array([ float(match.group(1))
-                                 , float(match.group(2))
-                                 , float(match.group(3))
-                                 , float(match.group(4))
-                                 , float(match.group(5))
-                                 , float(match.group(6))
-                                 , float(match.group(7))])
-    return np.array(q)
+                q = np.array([float(match.group(1))
+                             , float(match.group(2))
+                             , float(match.group(3))
+                             , float(match.group(4))
+                             , float(match.group(5))
+                             , float(match.group(6))
+                             , float(match.group(7))])
+    return q
 
 
 # https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
 class DataGenerator(tf.keras.utils.Sequence):
-    'Generates data for Keras'
-
     def __init__(self
-                 , list_ids
-                 , template_id
-                 , batch_size=32
-                 , dim=(256, 256, 256, 2)
-                 , n_channels=1
+                 , list_files
+                 , template_file
+                 , partition
+                 , batch_size=8
+                 , dim=(220, 220, 220)
+                 , n_channels=2
                  , n_regressors=7
                  , seed=None
                  , shuffle=True
                  , is_inference=False):
-        'Initialization'
         self.dim = dim
         self.batch_size = batch_size
-        self.list_ids = list_ids
-        self.template_id = template_id
+        self.list_files = list_files
+        self.partition = partition
+        self.template_file = template_file
+        self.n_samples = len(self.list_files)
+        self.indexes = np.arange(self.n_samples)
         self.n_channels = n_channels
         self.n_regressors = n_regressors
         self.seed = seed
         self.shuffle = shuffle
         self.is_inference = is_inference
+
         if not self.is_inference:
             self.on_epoch_end()
 
     def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_ids) / self.batch_size))
+        """Denotes the number of batches per epoch"""
+        return self.n_samples // self.batch_size
 
     def __getitem__(self, index):
-        'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
-
-        # Find list of IDs
-        list_ids_temp = [self.list_ids[k] for k in indexes]
-
+        """Generate one batch of data"""
+        list_files_batch = self.get_files_batch(index)
         # Generate data
-        data = self.__data_generation(list_ids_temp)
+        data = self.__data_generation(list_files_batch)
 
         return data
 
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_ids))
-        # needs s static variable to automatically increase the seed so the shuffle is not the same
-        if self.shuffle == True:
-            if self.seed is not None:
-                np.random.seed(self.seed)
-            np.random.shuffle(self.indexes)
+    def get_files_batch(self, index):
+        # Generate indexes of the batch
+        indexes = self.indexes_partition[index * self.batch_size:(index + 1) * self.batch_size]
+        # Find list of files for this batch
+        list_files_batch = [self.list_files[k] for k in indexes]
 
-    def __data_generation(self, list_ids_temp):
-        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
+        return list_files_batch
+
+    def _set_partition_idx(self):
+        """partition the indexes into train/valid/test data"""
+        if self.partition == "train":
+            range_idx = (0, int(0.7 * self.n_samples))
+        elif self.partition == "valid":
+            range_idx = (int(0.7 * self.n_samples), int(0.85 * self.n_samples))
+        elif self.partition == "test":
+            range_idx = (int(0.85 * self.n_samples), self.n_samples)
+        else:
+            print("Error: partition %s is not valid" % self.partition)
+        self.indexes_partition = self.indexes[range_idx[0]:range_idx[1]]
+
+    def on_epoch_end(self):
+        """Updates indexes after each epoch"""
+        if self.seed is not None:
+            np.random.seed(self.seed)
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+        self._set_partition_idx()
+
+    def normalize_img(self, img):
+        """
+        Normalize the image so that the mean value for each image
+        is 0 and the standard deviation is 1.
+        """
+        return (img - np.mean(img)) / np.std(img)
+
+    def __data_generation(self, list_files_batch):
+        """Generates data containing batch_size samples"""
         # Initialization
         x = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size, self.n_regressors), dtype=int)
+        y = np.empty((self.batch_size, self.n_regressors), dtype=np.float32)
 
         # Generate data
-        template = sitk.GetArrayFromImage(sitk.ReadImage(self.template_id + ".nii.gz", sitk.sitkFloat32))
-        for i, ID in enumerate(list_ids_temp):
+        template = sitk.GetArrayFromImage(sitk.ReadImage(self.template_file + ".nii.gz", sitk.sitkFloat32))
+        template = self.normalize_img(template)
+        for i, file in enumerate(list_files_batch):
             # Store sample
-            x[i, :, :, :, 0, ] = template
-            x[i, :, :, :, 1, ] = sitk.GetArrayFromImage(sitk.ReadImage(ID + ".nii.gz", sitk.sitkFloat32))
-            y[i, ] = load_file(ID + ".txt")
-            if y[i, ] is None:
-                self.is_inference = True
+            img = sitk.GetArrayFromImage(sitk.ReadImage(file + ".nii.gz", sitk.sitkFloat32))
+            img = self.normalize_img(img)
+            x[i, :, :, :, 0] = template
+            x[i, :, :, :, 1] = img
+            if self.is_inference is False:
+                y[i, ] = load_file(file + ".txt")
 
-        '''''
+        '''
         def z_normalize_img(self, img):
-            """
-            Normalize the image so that the mean value for each image
-            is 0 and the standard deviation is 1.
-            """
-            for channel in range(img.shape[-1]):
-                img_temp = img[..., channel]
-                img_temp = (img_temp - np.mean(img_temp)) / np.std(img_temp)
+        """
+        Normalize the image so that the mean value for each image
+        is 0 and the standard deviation is 1.
+        """
+        for channel in range(img.shape[-1]):
+            img_temp = img[..., channel]
+            img_temp = (img_temp - np.mean(img_temp)) / np.std(img_temp)
 
-                img[..., channel] = img_temp
+            img[..., channel] = img_temp
 
-            return img
-        '''''
+        return img
+        '''
 
         data = tuple([x, y])
-        if not self.is_inference:
-            data = tuple([x])
+        if self.is_inference:
+            data = x
 
         return data
