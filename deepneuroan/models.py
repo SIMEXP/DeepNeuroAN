6de5ac6e4535dec2a522d015c068054fb2cbaac6
ltetrel <loic.tetrel.pro@gmail.com>
2019-09-03 13:52:38 -0400

    custom keras layer channel wise 3d conv
    custom keras layer channel wise maxpool 3D
    
    optimisations, bugs correction and improvements

--- deepneuroan/models.py
+++ deepneuroan/models.py
@@ -3,13 +3,21 @@ import tensorflow as tf
 
 class ChannelwiseConv3D(tf.keras.layers.Layer):
     # just working for channel last
-    def __init__(self, filters=1, kernel_size=(1, 1, 1), dilation_rate=(1, 1, 1), padding='SAME', strides=(1, 1, 1),
-                 **kwargs):
+    def __init__(self
+                 , filters=1
+                 , kernel_size=(1, 1, 1)
+                 , dilation_rate=(1, 1, 1)
+                 , padding='SAME'
+                 , strides=(1, 1, 1)
+                 , kernel_initializer='glorot_uniform'
+                 , activation='relu'
+                 , **kwargs):
         self.filters = filters
         self.kernel_size = kernel_size
         self.dilation_rate = (1,) + dilation_rate + (1,)
         self.padding = padding
         self.strides = (1,) + strides + (1,)
+        self.kernel_initializer = kernel_initializer
 
         super(ChannelwiseConv3D, self).__init__(**kwargs)
 
@@ -17,18 +25,18 @@ class ChannelwiseConv3D(tf.keras.layers.Layer):
         # Create a trainable weight variable for this layer.
 
         # if the input length is > 5, then it has more than 1 feature map (number of kernel filters)
-        # this would happen usually wfor deeper layer but not the input layer
+        # this would happen usually for deeper layer but not the input layer
         self.n_input_fmps = 1
         if len(input_shape) > 5:
             self.n_input_fmps = int(input_shape[-2])
 
         self.kernel = self.add_weight(name='kernel',
                                       shape=self.kernel_size + (self.n_input_fmps,) + (self.filters,),
-                                      initializer='glorot_uniform',
+                                      initializer=self.kernel_initializer,
                                       trainable=True)
         self.bias = self.add_weight(name='bias',
                                     shape=(self.filters,),
-                                    initializer='glorot_uniform',
+                                    initializer=self.kernel_initializer,
                                     trainable=True)
         super(ChannelwiseConv3D, self).build(input_shape)  # Be sure to call this at the end
 
@@ -39,7 +47,7 @@ class ChannelwiseConv3D(tf.keras.layers.Layer):
             if len(x.shape) > 5:
                 split_input = tf.squeeze(split_input, axis=-1)
             out = tf.nn.conv3d(split_input
-                               , self.kernel
+                               , filter=self.kernel
                                , strides=self.strides
                                , padding=self.padding
                                , dilations=self.dilation_rate)
@@ -53,7 +61,47 @@ class ChannelwiseConv3D(tf.keras.layers.Layer):
         return output_shape
 
 
-def encode_block_channelwise(x, n_filters, name, params_conv, params_layer):
+class ChannelwiseMaxpool3D(tf.keras.layers.Layer):
+    # just working for channel last
+    def __init__(self
+                 , pool_size=(1, 1, 1)
+                 , padding="SAME"
+                 , strides=None
+                 , **kwargs):
+        self.pool_size = pool_size
+        self.padding = padding
+        self.strides = strides
+        if strides is None:
+            self.strides = pool_size
+        super(ChannelwiseMaxpool3D, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(ChannelwiseMaxpool3D, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, x):
+        outputs = []
+        split_inputs = tf.split(x, x.shape[-1], axis=-1)
+        for split_input in split_inputs:
+            if len(x.shape) > 5:
+                split_input = tf.squeeze(split_input, axis=-1)
+            out = tf.nn.max_pool3d(split_input
+                                   , ksize=self.pool_size
+                                   , strides=self.strides
+                                   , padding=self.padding)
+            outputs += [out]
+        outputs = tf.stack(outputs, axis=-1)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        vol_shape = (input_shape[:3] - self.pool_size)/self.strides + 1
+        output_shape = (vol_shape,) + input_shape[3:]
+        return output_shape
+
+
+def rigid_metric(y_true, y_pred):
+    return tf.reduce_mean(tf.abs(y_true - y_pred), axis=0)
+
+def encode_block_channelwise(x, filters, name, params_conv, params_layer):
     '''''
     One encoding block contains:
     - 3x3 convolution on each voxel
@@ -67,12 +115,14 @@ def encode_block_channelwise(x, n_filters, name, params_conv, params_layer):
     https://stackoverflow.com/questions/36817868/tensorflow-how-to-pool-over-depth
     '''''
 
-    x = ChannelwiseConv3D(name=name + "_conv", filters=n_filters, **params_conv)(x)
+    x = ChannelwiseConv3D(name=name + "_conv", filters=filters, **params_conv)(x)
+    x = tf.keras.layers.Activation(params_conv["activation"])(x)
     if params_layer["batch_norm"]:
         x = tf.keras.layers.BatchNormalization(name=name + "_bn")(x)
     if params_layer["dropout"] > 0:
         x = tf.keras.layers.Dropout(rate=params_layer["dropout"], name=name + "_dropout", seed=params_layer["seed"])(x)
-    x = tf.keras.layers.MaxPool3D(pool_size=params_layer["pool_size"], padding=params_layer["padding"], name=name + "_maxpool")(x)
+    x = ChannelwiseMaxpool3D(
+        pool_size=params_layer["pool_size"], padding=params_layer["padding"], name=name + "_maxpool")(x)
 
     return x
 
@@ -97,13 +147,13 @@ def regression_block(x, n_units, name, params_dense, params_layer):
 def rigid_concatenated(kernel_size=(3, 3, 3)
                        , pool_size=(2, 2, 2)
                        , activation="relu"
-                       , padding="valid"
+                       , padding="VALID"
                        , batch_norm=True
                        , dropout=0.1
                        , seed=0
                        , growth_rate=2
                        , filters=4
-                       , units=1096
+                       , units=1024
                        , n_encode_layers=6
                        , n_regression_layers=4):
     '''''
@@ -111,8 +161,9 @@ def rigid_concatenated(kernel_size=(3, 3, 3)
     The input volume is the concatenation of the 2 volumes to register.
     '''''
 
-    params_conv = dict(kernel_size=kernel_size, kernel_initializer='glorot_uniform', activation=activation, padding=padding)
-    params_dense = dict(kernel_initializer='glorot_uniform', activation=activation)
+    k_init = tf.glorot_uniform_initializer(seed=seed)
+    params_conv = dict(kernel_size=kernel_size, kernel_initializer=k_init, activation=activation, padding=padding)
+    params_dense = dict(kernel_initializer=k_init, activation=activation)
     params_layer = dict(pool_size=pool_size, padding=padding, batch_norm=batch_norm, dropout=dropout, seed=seed)
 
     '''''
@@ -142,24 +193,21 @@ def rigid_concatenated(kernel_size=(3, 3, 3)
     output : 2x2x2x2 x 256 = 4096 params
     '''''
 
-    input = tf.keras.Input(shape=(None, None, None, 2))
+    inp = tf.keras.Input(shape=(220, 220, 220, 2), dtype=tf.float32)
     # encoder part
     for i in range(n_encode_layers):
-        features = input if i == 0 else features
-        fmps = filters * growth_rate**i
-        x = encode_block_channelwise(features, fmps, "encode%02d" % i, params_conv, params_layer)
+        features = inp if i == 0 else features
+        layer_filters = int(filters * growth_rate**i)
+        features = encode_block_channelwise(features, layer_filters, "encode%02d" % i, params_conv, params_layer)
     regression = tf.keras.layers.Flatten()(features)
 
     # regression
     for i in range(n_regression_layers):
-        unts = units // growth_rate ** i
-        regression = regression_block(regression, unts, "regress%02d" % i, params_dense, params_layer)
-    output = tf.keras.layers.Dense(units=7
-                                   , kernel_initializer=params_dense["kernel_initializer"]
-                                   , activation=None)(regression)
+        layer_units = int(units // growth_rate**i)
+        regression = regression_block(regression, layer_units, "regress%02d" % i, params_dense, params_layer)
+    output = tf.keras.layers.Dense(
+        units=7, kernel_initializer=params_dense["kernel_initializer"], activation=None)(regression)
 
-    model = tf.keras.models.Model(inputs=[input], outputs=[output])
-
-
+    model = tf.keras.models.Model(inputs=[inp], outputs=[output])
 
     return model
