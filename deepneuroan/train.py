notebook folder is now included in git clip gradient norm to 1 (to avoid exploding gradient) fix learning rate print

deepneuroan/train.py
@@ -91,7 +91,7 @@ class Training:
                + "\n\t units : %d" % self._units \
                + "\n\t number of encoding layer : %d" % self._encode_layers \
                + "\n\t number of regression layer : %d" % self._regression_layers \
-               + "\n\t learning rate : %d" % self._lr
+               + "\n\t learning rate : %f" % self._lr
 
     def _set_data_dir(self, data_dir=None):
         if data_dir is None:
@@ -193,7 +193,7 @@ class Training:
 
         # model building
         model = self._build_model()
-        model.compile(optimizer=tf.keras.optimizers.Adam(lr=self._lr)
+        model.compile(optimizer=tf.keras.optimizers.Adam(lr=self._lr, clipnorm=1)
                       , loss=tf.keras.losses.mean_squared_error
                       , metrics=["mae"])
 