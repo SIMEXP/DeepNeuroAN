76ba92e1d336d0a9c84d3427721993b27eadefeb
ltetrel <loic.tetrel.pro@gmail.com>
2019-09-25 16:25:50 -0400

    * Optimized data_generator :   - multiprocessing with shared memory (best between pool , pool with shared, multithreading and multiprocess)   check test_load.ipynb for benchmarks   - optimization regarding batch size and available cores   - Data generator now working for inference (need to remove template reading..)

--- deepneuroan/data_generator.py
+++ deepneuroan/data_generator.py
@@ -34,7 +34,7 @@ class DataGenerator(tf.keras.utils.Sequence):
     def __init__(self
                  , list_files
                  , template_file
-                 , partition
+                 , partition="infer"
                  , batch_size=8
                  , dim=(220, 220, 220)
                  , n_channels=2
@@ -69,7 +69,7 @@ class DataGenerator(tf.keras.utils.Sequence):
         """Generate one batch of data"""
         list_files_batch = self.get_files_batch(index)
         # Generate data
-        data = self.__data_generation(list_files_batch)
+        data = self.__mp_data_generation(list_files_batch)
 
         return data
 
@@ -89,6 +89,8 @@ class DataGenerator(tf.keras.utils.Sequence):
             range_idx = (int(0.7 * self.n_samples), int(0.85 * self.n_samples))
         elif self.partition == "test":
             range_idx = (int(0.85 * self.n_samples), self.n_samples)
+        elif self.partition == "infer":
+            range_idx = (0, self.n_samples)
         else:
             print("Error: partition %s is not valid" % self.partition)
 
@@ -106,31 +108,93 @@ class DataGenerator(tf.keras.utils.Sequence):
         """
         return (img - np.mean(img)) / np.std(img)
 
-    def load_img(self, file):
+    def shared_to_numpy(self, shared_arr, shape, dtype):
+        """Get a NumPy array from a shared memory buffer, with a given dtype and shape.
+        No copy is involved, the array reflects the underlying shared buffer."""
+        sz = int(np.product(shape))
+        dtype = np.dtype(dtype)
+        return np.frombuffer(shared_arr, dtype=dtype, count=sz).reshape(shape)
+
+    def create_shared_array(self, shape, dtype):
+        """Create a new shared array. Return a tuple of (shared array pointer, shape, npdtype),
+        and a NumPy array view to it. Note that the buffer values are not initialized.
+        """
+        # Get a ctype type from the NumPy dtype.
+        cdtype = np.ctypeslib.as_ctypes_type(dtype)
+        # Create the RawArray instance.
+        shared_arr = mp.RawArray(cdtype, int(np.prod(shape)))
+        # Get a NumPy array view.
+        arr = self.__shared_to_numpy(shared_arr, shape, dtype)
+        return (shared_arr, shape, dtype,), arr
+
+    def worker_load_data_train(self, i, file, s_data_x, s_data_y):
+        data_x = self.shared_to_numpy(*s_data_x)
+        data_y = self.shared_to_numpy(*s_data_y)
+
+        data_x[i, :, :, :, 0] = self.template
         img = sitk.GetArrayFromImage(sitk.ReadImage(file + ".nii.gz", sitk.sitkFloat32))
         img = self.normalize_img(img)
+        data_x[i, :, :, :, 1] = img
+        data_y[i, ] = load_file(file + ".txt")
 
-        return img
+    def worker_load_data_infer(self, i, file, s_data_x):
+        data_x = self.shared_to_numpy(*s_data_x)
 
-    def __data_generation(self, list_files_batch):
+        img = sitk.GetArrayFromImage(sitk.ReadImage(file + ".nii.gz", sitk.sitkFloat32))
+        img = self.normalize_img(img)
+        data_x[i, :, :, :, 0] = img
+
+    def create_processes(self, nb_proc, files, shared_mem, curr_proc_batch=0):
+        # will not create more processes than available cores
+        nb_proc = min(mp.cpu_count(), nb_proc)
+
+        #processes creation
+        processes = []
+        for i in range(nb_proc):
+            k = curr_proc_batch * nb_proc + i
+            if not self.is_inference:
+                process = mp.Process(target=self.worker_load_data_train, args=(k, files[k], *shared_mem))
+            else:
+                process = mp.Process(target=self.worker_load_data_infer, args=(k, files[k], *shared_mem))
+            processes.append(process)
+            process.start()
+
+        # waiting for processes to end
+        for process in processes:
+            process.join()
+
+    def __mp_data_generation(self, list_files_batch):
         """Generates data containing batch_size samples"""
-        # Initialization
-        x = np.empty((self.batch_size, *self.dim, self.n_channels), dtype=np.float32)
-        y = np.empty((self.batch_size, self.n_regressors), dtype=np.float32)
 
-        pool = mp.Pool(min(mp.cpu_count(), self.batch_size))
-        imgs = pool.map(self.load_img, [file for file in list_files_batch])
-        pool.close()
+        # shared memory pointers
+        s_data_x = None
+        s_data_y = None
+        s_data_x, data_x = self.create_shared_array(shape=(self.batch_size, *self.dim, self.n_channels), dtype=np.float32)
+        if not self.is_inference:
+            s_data_y, data_y = self.create_shared_array(shape=(self.batch_size, self.n_regressors), dtype=np.float32)
+        s_mem = (s_data_x, s_data_y)
 
-        for i, file in enumerate(list_files_batch):
-            # storing the template and img
-            x[i, :, :, :, 0] = self.template
-            x[i, :, :, :, 1] = imgs[i]
-            if self.is_inference is False:
-                y[i, ] = load_file(file + ".txt")
+        # If there batch size is bigger than available cores (proc_batches > 0),
+        # then we need to manage the batch loading among the available cores.
+        # Otherwise, there is enough cpus for the batch size, so each sample in the batch can be loaded by one cpu
+        cores = mp.cpu_count()
+        p_batches = self.batch_size // cores
+        remainder = self.batch_size % cores
+        if p_batches > 0:
+            for i in range(p_batches):
+                print("Loading %d/%d mini-batch with %d cpus" % (i, p_batches if remainder > 0 else p_batches-1, cores))
+                self.create_processes(nb_proc=cores, files=list_files_batch, shared_mem=s_mem, curr_proc_batch=i)
+            # and now the remaining processes
+            if remainder > 0:
+                print("Loading %d/%d mini-batch with %d cpus" % (p_batches, p_batches, cores))
+                self.create_processes(
+                    nb_proc=remainder, files=list_files_batch, shared_mem=s_mem, curr_proc_batch=p_batches)
+        else:
+            print("Loading batch with %d cpus" % self.batch_size)
+            self.create_processes(nb_proc=self.batch_size, files=list_files_batch, shared_mem=s_mem)
 
-        data = tuple([x, y])
+        data = tuple([data_x, data_y])
         if self.is_inference:
-            data = x
+            data = (data_x, )
 
         return data
